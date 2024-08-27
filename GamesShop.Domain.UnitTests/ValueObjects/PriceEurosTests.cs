f2cceb62ded1dc2b51ef9cb20215dbe3a8879f92
Jaime Daniel Delgado Ortega <jdaniel1987@hotmail.com>
2024-08-27 12:44:20 +0200

    Add Domain unit tests Add Application unit tests

---
+++ GamesShop.Domain.UnitTests/ValueObjects/PriceEurosTests.cs
@@ -0,0 +1,34 @@
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using GamesShop.Domain.ValueObjects;
+
+namespace GamesShop.Domain.UnitTests.ValueObjects;
+
+public class PriceEurosTests
+{
+    [Theory, AutoData]
+    public void Create_ShouldInitializePriceEurosCorrectly(
+        double value)
+    {
+        // Act
+        var priceEuros = PriceEuros.Create(value);
+
+        // Assert
+        priceEuros.Value.Should().Be(value);
+    }
+
+    [Theory, AutoData]
+    public void ToString_ShouldReturnValueAsString(
+        double value)
+    {
+        // Arrange
+        var priceEuros = PriceEuros.Create(value);
+
+        // Act
+        var result = priceEuros.ToString();
+
+        // Assert
+        result.Should().Be(value.ToString());
+    }
+}
+
